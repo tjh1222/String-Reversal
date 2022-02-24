b27464b21a6df870e54508194e984656c088f6f3
Tyler Hurley <tylerjohnhurley@gmail.com>
2022-02-24 11:44:25 -0500

    Finished helper function reverse_words that will be used to reverse the order of words in the provided string

--- reverse-helpers.py
+++ reverse-helpers.py
@@ -1,2 +1,12 @@
 def reverse_string(string):
   return string[::-1]
+
+
+
+def reverse_words(string):
+  lines = string.split("\n")
+  for i in range(len(lines)):
+    words = lines[i].split(" ")
+    lines[i] = " ".join(words[::-1])
+  return "\n".join(lines[::-1])
+  
