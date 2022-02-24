c84d1e54c2eea1d1dfe8f39b43f84319bfcd324f
Tyler Hurley <tylerjohnhurley@gmail.com>
2022-02-24 13:07:23 -0500

    Added the optional flag -f which enables the input string to be read from a file. This means that the required string that is passed into this program as an argument can now represent both the string to be reversed as well as a file path that contains the string to be reversed.

--- string_reversal.py
+++ string_reversal.py
@@ -1,5 +1,6 @@
 from reverse_helpers import reverse_string, reverse_words
 import argparse
+import sys
 
 # Create command line argument parser
 parser = argparse.ArgumentParser(description="String Manipulation Program")
@@ -29,10 +30,33 @@ parser.add_argument(
   help="reverse words in provided string"
 )
 
+# Setup Optional flag which specifies that the positional argument string is a path to a readable file instead of the string to be reversed
+parser.add_argument(
+  "-f",
+  "--file",
+  action="store_true",
+  help="get input string from file"
+)
+
+
+
+def get_string_from_file(path):
+  with open(path) as f:
+    text = f.read()
+  return text
+
 if (__name__ == "__main__"):
   args = parser.parse_args()
   input_string = args.string
   result = ""
+
+  if (args.file):
+    try:
+      input_string = get_string_from_file(input_string)
+    except:
+      print("There was a problem accessing the file. Make sure you provided a valid path to a readable file")
+      sys.exit()
+  
 
   if (args.reverse_by == "char"):
     result = reverse_string(input_string)
